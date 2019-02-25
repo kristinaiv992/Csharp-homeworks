050a677dfc970ffdbf89c6ecc7586ac1a8dc9ce8
kristinaiv992 <45533932+kristinaiv992@users.noreply.github.com>
2019-02-25 23:12:47 +0100

    Add files via upload

---
+++ homework1/christmasTree2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_kristinaivanova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Christmas Tree\n");
+            var oldColor = Console.ForegroundColor;
+            int i, j, k, rows, cols;
+            for (i = 0; i <= 4; i++)
+            {
+                for (j = i; j < 5; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (k = 1; k < (i * 2); k++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            for(rows=0; rows<2; rows++)
+            {
+                for(cols=0; cols<3; cols++)
+                {
+                    Console.Write(" ");
+                }
+                for (k = 0; k < 2; k++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = oldColor;
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
---
+++ homework1/christmastree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1
+{
+    class Program
+    {
+        static void Main()
+        {
+           int i, j;
+            int space = 20;
+            int stars = 1;
+            var oldColor = Console.ForegroundColor;
+            for (i = 0; i < space; i++)
+            {
+                
+                for (j = 0; j < space; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 0; j < stars; j++)
+                {
+                Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("* ");
+                }
+                    stars++;
+                Console.WriteLine();
+                space--;   
+            }
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("                    ||");
+            Console.WriteLine("                    ||");
+            Console.ForegroundColor = oldColor;
+            Console.ReadKey();
+
+        }
+        }
+    }
+
