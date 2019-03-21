75e4db04fd22af6fde8b813c64f3cc28d3b57b68
Kristina <kristinaiv992@gmail.com>
2019-03-21 23:16:37 +0100

    Added homework 3 phonebook

---
+++ homework3/phonebook/Names.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phonebook
+{
+    public class Names
+    {
+      public Dictionary<string, int> PhoneBook = new Dictionary<string, int>()
+        {
+            {"Kika", 9990045},
+            {"Mitko", 9956745},
+            {"Slobodan", 9911245},
+            {"John", 9900745},
+            {"Mike", 9999045}
+        };
+        
+        
+    }
+}
---
+++ homework3/phonebook/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phonebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Phone book dictionary with 5 names and phone numbers
+            //Ask the person to enter a name
+            //try to find that name in the phone book
+            //if you can find it print in the console
+            //if you cant print an error message
+
+            var DictionaryNames = new Names();
+           
+            bool stop = false;
+            string input;
+            char answer;
+
+            do
+            {
+                bool found = false;
+                Console.WriteLine("Please enter the person you want to search for?");
+                input = Console.ReadLine();
+
+                foreach (string name in DictionaryNames.PhoneBook.Keys)
+                {
+                    if (input == name || input == name.ToUpper() || input == name.ToLower())
+                    {
+                        found = true;
+                        Console.WriteLine($"The phone number of {input} is {DictionaryNames.PhoneBook[name]} ");
+                    }
+                }
+               
+                if (!found)
+                {
+                    Console.WriteLine($"the person {input} is not present in your Phonebook. Do you want to add that person to your PhoneBook? press Y/N");
+                    answer = Console.ReadKey().KeyChar;
+
+                    if (answer =='n' || answer == 'N')
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter your person number?");
+                        var number = int.Parse(Console.ReadLine());
+
+                        DictionaryNames.PhoneBook.Add(input, number);
+                    }
+
+
+                }
+                Console.WriteLine("Do you want to search again? Press Y/n");
+
+                var answer2 = char.Parse(Console.ReadLine());
+                if (answer2 == 'n' || answer2 == 'N')
+                {
+                    stop = true;
+                    break;
+                }               
+
+            } while (stop == false);
+
+           
+        }
+    }
+           
+}
